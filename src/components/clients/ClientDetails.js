7c6ccb73bb76f7aeaa32cc7566291b12703a5a77
Daniel C. Landon Jr <daniel.landonjr@gmail.com>
2018-09-19 15:41:30 -0400

    Added ability to update balance. Added delete client capability.

--- src/components/clients/ClientDetails.js
+++ src/components/clients/ClientDetails.js
@@ -11,8 +11,72 @@ import Spinner from '../layout/Spinner';
 import BackToDashboard from '../layout/BackToDashboard';
 
 class ClientDetails extends Component {
+  state = {
+    showBalanceUpdate: false,
+    balanceUpdateAmount: ''
+  };
+
+  onDeleteClient = event => {
+    const { client, firestore, history } = this.props;
+
+    firestore.delete({ collection: 'clients', doc: client.id }).then(() => {
+      history.push('/');
+    });
+  };
+
+  // this is required so that we can update the values of the controls
+  onChange = event => {
+    this.setState({ [event.target.name]: event.target.value });
+  };
+
+  balanceSubmit = event => {
+    event.preventDefault();
+
+    // descructuring is just making things a bit simpler...
+    // i could get the same results if i typed this.props.client or this.props.firestore or this.state.balanceUpdateAmount
+    const { client, firestore } = this.props;
+    const { balanceUpdateAmount } = this.state;
+
+    const clientUpdate = {
+      balance: parseFloat(balanceUpdateAmount)
+    };
+
+    // update value in firestore
+    firestore.update({ collection: 'clients', doc: client.id }, clientUpdate);
+  };
+
   render() {
     const { client } = this.props;
+    const { showBalanceUpdate, balanceUpdateAmount } = this.state;
+
+    let balanceForm = null;
+
+    if (showBalanceUpdate) {
+      balanceForm = (
+        <form onSubmit={this.balanceSubmit}>
+          <div className="input-group">
+            <input
+              type="text"
+              className="form-control"
+              name="balanceUpdateAmount"
+              placeholder="Add New Amount"
+              value={balanceUpdateAmount}
+              onChange={this.onChange}
+            />
+            <div className="input-group-append">
+              <input
+                type="submit"
+                value="Update"
+                className="btn btn-outline-dark"
+              />
+            </div>
+          </div>
+        </form>
+      );
+    } else {
+      // form not showing
+      balanceForm = null;
+    }
 
     if (client) {
       return (
@@ -26,7 +90,12 @@ class ClientDetails extends Component {
                 <Link to={`client/edit/${client.id}`} className="btn btn-dark">
                   Edit
                 </Link>
-                <button className="btn btn-danger">Delete</button>
+                <button
+                  onClick={this.onDeleteClient}
+                  className="btn btn-danger"
+                >
+                  Delete
+                </button>
               </div>
             </div>
           </div>
@@ -54,10 +123,25 @@ class ClientDetails extends Component {
                       })}
                     >
                       ${parseFloat(client.balance).toFixed(2)}
-                    </span>
+                    </span>{' '}
+                    <small>
+                      <a
+                        href="#!"
+                        onClick={() =>
+                          this.setState({
+                            // toggle state value
+                            showBalanceUpdate: !this.state.showBalanceUpdate
+                          })
+                        }
+                      >
+                        <i className="fas fa-pencil-alt" />
+                      </a>
+                    </small>
                   </h3>
 
-                  {/* @todo balance -form */}
+                  {/* if the showBalanceUpdate is false then balance update is null and nothing shows
+                   */}
+                  {balanceForm}
                 </div>
               </div>
 
